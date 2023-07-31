0ce0e6bc7fdcd367fb083aefc26429aee31c4f93
김군순 <rlarnstns@gmail.com>
2023-07-31 16:50:26 +0900

    2

---
+++ JS/01 Basic/ex03output.js
@@ -0,0 +1,24 @@
+// 주석 입력
+/*
+다중 주석 입력
+*/
+
+// 1. HTML 문서 내 출력
+document.write('김군순 짱!');
+
+document.write('<h1>김군순 바보!</h1>');
+
+document.write('<h2>');
+document.write('김군순 별로');
+document.write('</h2>');
+
+
+// 2. console창 출력
+// log → 자동완성 이용!
+// 개발자도구(F12)>콘솔
+console.log('김군순 콘솔 출력');
+console.log('log 자동완성');
+
+// 3. 알림 팝업창 출력
+alert('김군순 특가 세일!💥');
+alert('💥김훈종 오늘부터 매일 강의실 연장사용!💥');
